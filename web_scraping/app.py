d96feb5b3aebd13d9e30533c32e92ff614760e4b
OG.Saver <KevinWang@OGSaverdeMacBook-Air.local>
2019-07-24 23:46:21 -0400

    revise search feature

--- web_scraping/app.py
+++ web_scraping/app.py
@@ -13,8 +13,7 @@ def main_post():
     job_name = job_name.lower()
     df = pd.read_csv("%s/data/Job_search.csv" % main_path(), index_col=None)
     df['Job_Name'] = df['Job_Name'].str.lower()
-    job_name = job_name.split(" ")
-    df = df[df['Job_Name'].str.contains(r'\b(?:{})\b'.format('|'.join(job_name)))]
+    df = df[df['Job_Name'].str.contains(job_name)]
     df['Job_link'] = df['Job_link'].apply(lambda x: '<a href="{0}">{0}</a>'.format(x))
     old_width = pd.get_option('display.max_colwidth')
     pd.set_option('display.max_colwidth', -1)
